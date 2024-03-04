74da461d90599021483a4369db38f80cd68f4827
edurebolledo21 <edurebolledo68@gmail.com>
2024-03-04 00:53:14 -0400

    update hosting test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      htmlAttrs: {
-        lang: "en",
-      },
-    },
-  },
-  css: ["~/assets/css/main.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  modules: [
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          Poppins: [400, 500, 600, 700],
-          "Chakra Petch": [300, 400, 500, 600, 700],
-        },
-      },
-    ],
-    "nuxt-icon",
-    "@vueuse/nuxt",
-    "@formkit/nuxt",
-  ],
+  devtools: { enabled: true },
 });
